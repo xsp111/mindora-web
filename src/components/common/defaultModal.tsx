8e1d8e5559bc5f9739ea40538ed35feaea1b5616
xusipeng <2996124387@qq.com>
2026-05-19 13:44:37 +0800

    fix: 弹窗每次关闭销毁组件

--- src/components/common/defaultModal.tsx
+++ src/components/common/defaultModal.tsx
@@ -3,7 +3,13 @@ import { Modal, type ModalProps } from 'antd';
 export default function DefaultModal(props: ModalProps) {
 	const { children, ...restProps } = props;
 	return (
-		<Modal centered footer={null} maskClosable={false} {...restProps}>
+		<Modal
+			destroyOnHidden
+			centered
+			footer={null}
+			maskClosable={false}
+			{...restProps}
+		>
 			{children}
 		</Modal>
 	);
